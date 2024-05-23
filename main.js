8fcda16dd6f41dc53eed61eb41bb3933e2e3ef41
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-05-23 13:39:50 -0300

    Adicionando conteúdo sobre Eventos e Lógicas.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado!');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -13,5 +20,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onclick = function () {
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (event) {
+
+        if (event.code === 'Enter' || event.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 }
